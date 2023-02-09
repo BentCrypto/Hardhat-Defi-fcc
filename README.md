e3a0f52981507191e1dfabbb0a6fcc12d85622a5
BentCrypto <dagenscrypto@gmail.com>
2023-02-09 11:33:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Learning to do a algorythmic deposit, borrow and repay on aave protocol with Patrick Collins https://freecodecamp.org/
+
+
+1. Deposit collateral ETH/ WETH ✅
+2. Borrow another asset: DAI✅
+3. Repay the DAI✅
+
---
+++ scripts/aaveBorrow.js
@@ -0,0 +1,114 @@
+const { getNamedAccounts, ethers } = require("hardhat")
+const { getWeth, AMOUNT } = require("../scripts/getWeth")
+
+async function main() {
+  await getWeth()
+  const { deployer } = await getNamedAccounts()
+  // abi, address
+  // Lending Pool Address Provider: 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+  // Lending Pool:
+  const lendingPool = await getLendingPool(deployer)
+  console.log(`LendingPool address ${lendingPool.address}`)
+
+  // LendingPool address: 0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9
+
+  //deposit
+  const wethTokenAddress = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+  //approve
+  await approveErc20(wethTokenAddress, lendingPool.address, AMOUNT, deployer)
+  console.log("Depositing ....")
+  await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0)
+  console.log("Deposited")
+  let { availableBorrowsETH, totalDebtETH } = await getBorrowUserData(
+    lendingPool,
+    deployer
+  )
+  const daiPrice = await getDaiPrice()
+  const amountDaiToBorrow =
+    availableBorrowsETH.toString() * 0.95 * (1 / daiPrice.toNumber())
+  console.log(`You can borrow ${amountDaiToBorrow} DAI`)
+  const amountDaiToBorrowWei = ethers.utils.parseEther(
+    amountDaiToBorrow.toString()
+  )
+
+  // availableBorrowsETH in DAI, what the conversion rate on DAI is ?
+  // borrow time!
+  // how much we have borrowed, how much we have in colleteral, how much we can borrow
+  const daiTokenAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
+  await borrowDai(daiTokenAddress, lendingPool, amountDaiToBorrowWei, deployer)
+  await getBorrowUserData(lendingPool, deployer)
+  await repay(amountDaiToBorrowWei, daiTokenAddress, lendingPool, deployer)
+}
+
+async function repay(amount, daiAddress, lendingPool, account) {
+  await approveErc20(daiAddress, lendingPool.address, amount, account)
+  const repayTx = await lendingPool.repay(daiAddress, amount, 1, account)
+  await repayTx.wait(1)
+  console.log("Repaid!")
+}
+
+async function borrowDai(daiAddress, lendingPool, amountDaiToBorrow, account) {
+  const borrowTx = await lendingPool.borrow(
+    daiAddress,
+    amountDaiToBorrow,
+    1,
+    0,
+    account
+  )
+  await borrowTx.wait(1)
+  console.log("You've borrowed!")
+}
+
+async function getDaiPrice() {
+  const daiEthPriceFeed = await ethers.getContractAt(
+    "AggregatorV3Interface",
+    "0x773616E4d11A78F511299002da57A0a94577F1f4"
+  )
+  const price = (await daiEthPriceFeed.latestRoundData())[1]
+  console.log(`The DAI/ETH price is ${price.toString()}`)
+  return price
+}
+
+async function getBorrowUserData(lendingPool, account) {
+  const { totalCollateralETH, totalDebtETH, availableBorrowsETH } =
+    await lendingPool.getUserAccountData(account)
+  console.log(`You have ${totalCollateralETH} worth of ETH deposited.`)
+  console.log(`You have ${totalDebtETH} worth of ETH borrowed.`)
+  console.log(`You have ${availableBorrowsETH} worth of ETH`)
+  return { availableBorrowsETH, totalDebtETH }
+}
+
+async function getLendingPool(account) {
+  const lendingPoolAddressProvider = await ethers.getContractAt(
+    "ILendingPoolAddressesProvider",
+    "0xb53c1a33016b2dc2ff3653530bff1848a515c8c5",
+    account
+  )
+  const lendingPoolAddress = await lendingPoolAddressProvider.getLendingPool()
+  const lendingPool = await ethers.getContractAt(
+    "ILendingPool",
+    lendingPoolAddress,
+    account
+  )
+  return lendingPool
+}
+
+async function approveErc20(
+  erc20Address,
+  spenderAddress,
+  amountToSpend,
+  account
+) {
+  const erc20Token = await ethers.getContractAt("IERC20", erc20Address, account)
+
+  const tx = await erc20Token.approve(spenderAddress, amountToSpend)
+  await tx.wait(1)
+  console.log("Approved!")
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
